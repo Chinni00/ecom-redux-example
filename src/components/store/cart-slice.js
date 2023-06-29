c3456946ddd130aa37deebbf148460f87540316e
chinni00 <chinni0437@gmail>
2023-06-29 18:32:45 +0530

    added a new reducer in cart-slice

--- src/components/store/cart-slice.js
+++ src/components/store/cart-slice.js
@@ -1,11 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -19,24 +24,23 @@ const cartSlice = createSlice({
           name: newItem.title,
         });
       } else {
-        existingItem.quantity++,
-          (existingItem.totalPrice = existingItem.totalPrice + newItem.price);
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart(state,action) {
-        const id = action.payload;
-        const existingItem =state.items.find(item=>item.id === id)
-        state.totalQuantity--;
-        if(existingItem.quantity===1){
-          state.items=state.items.filter(item=> item.id !==id)
-        }else{
-            existingItem.quantity--
-            existingItem.totalPrice= existingItem.totalPrice-existingItem.price
-        }
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
     },
   },
 });
 
-
 export const cartActions = cartSlice.actions;
+
 export default cartSlice;
